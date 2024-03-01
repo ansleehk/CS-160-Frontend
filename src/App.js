1288c88b05c05198d34b2240b9d6d9ab857c5d91
leeanson19 <leeanson19@gmail.com>
2024-02-29 20:52:29 -0800

    modify welcome message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CS-160 Frontend
         </p>
         <a
           className="App-link"
