fd28084c8eed83a582aca67ba0f06b43d4186a40
Anson <leeanson19@gmail.com>
2024-03-03 01:31:40 +0000

    Add production message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CS-160 Frontend
+          CS-160 Frontend Production
         </p>
         <a
           className="App-link"
